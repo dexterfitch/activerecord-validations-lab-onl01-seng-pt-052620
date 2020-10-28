ad9d977520872b37aea4fcc12c4d13fe4e967d93
Dexter Fitch <fitch.dexter@gmail.com>
2020-10-28 02:14:58 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,12 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, presence: true, if: :clickbait?
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  validate :clickbait?
+  # validate :clickbait?
 
   def clickbait?
-    content.include?("Won't Believe", "Secret", "Top %w(/\d/)", "Guess")
+    content.include?(["Won't Believe", "Secret", "Top %w(/\d/)", "Guess"])
   end
 
 end
