da9c510eae9e454c1c5cd56dafac82c534c4f544
Dexter Fitch <fitch.dexter@gmail.com>
2020-10-28 02:20:54 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,9 @@ class Post < ActiveRecord::Base
   validate :clickbait?
 
   def clickbait?
-    title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top %w(/\d/)") || title.include?("Guess")
+    if title
+      title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top %w(/\d/)") || title.include?("Guess")
+    end
   end
 
 end
