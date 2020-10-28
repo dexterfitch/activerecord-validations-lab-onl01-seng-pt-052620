7d3dbc4890eb94aa2cd061e1294b0d204e704e2c
Dexter Fitch <fitch.dexter@gmail.com>
2020-10-28 02:03:50 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, presence: true, inclusion: { in: %w(small medium large)
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates :category, inclusion: { in: %w(Won't Believe Secret Top Guess) }
 end
