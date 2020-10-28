c90eef949378fdee1289bb27ccb4d3c68041dc2e
Dexter Fitch <fitch.dexter@gmail.com>
2020-10-28 02:17:40 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true, if: :clickbait?
+  validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  # validate :clickbait?
+  validate :clickbait?
 
   def clickbait?
     content.include?("Won't Believe") || content.include?("Secret") || content.include?("Top %w(/\d/)") || content.include?("Guess")
